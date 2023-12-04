9041890fb07618f56fbcaf3c5d9afef0df62e0a7
Tung Lok Wan <wantunglok@gmail.com>
2023-12-05 03:13:39 +1100

    commit

--- research/github-security-policy/wanlok.js
+++ research/github-security-policy/wanlok.js
@@ -37,74 +37,20 @@ prepareAndUpdateUI = function() {
 getContentDiv = function() {
     return document.getElementById(elementId).children[1].children[0];
 }
-        
-getCSV = function(csvText) {
-    rows = [];
-    lines = csvText.replace('\r', '').split('\n');
-    columnNames = lines[0].split(',');
-    for (i = 1; i < lines.length; i++) {
-        row = {};
-        line = lines[i].split(',');
-        if (columnNames.length == line.length) {
-            for (j = 0; j < columnNames.length; j++) {
-                row[columnNames[j]] = line[j];
-            }
-            rows.push(row);
-        }
-    }
-    return [columnNames, rows];
-}
-
-
-dummy2 = function(line) {
-    slices = line.split(',');
-    // if (columnNames.length == slices.length) {
-        // for (j = 0; j < slices.length; j++) {
-        //     row[columnNames[j]] = line[j];
-        // }
-        // rows.push(row);
-    // }
-
-}
-
-CSVtoArray = function(text) {
-    var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
-    var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
-    // Return NULL if input string is not well formed CSV string.
-    if (!re_valid.test(text)) return null;
-    var a = [];                     // Initialize array to receive values.
-    text.replace(re_value, // "Walk" the string using replace with callback.
-        function(m0, m1, m2, m3) {
-            // Remove backslash from \' in single quoted values.
-            if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
-            // Remove backslash from \" in double quoted values.
-            else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
-            else if (m3 !== undefined) a.push(m3);
-            return ''; // Return empty string.
-        });
-    // Handle special case of empty last value.
-    if (/,\s*$/.test(text)) a.push('');
-    return a;
-}
 
 write = function(title, text) {
+    const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
     lines = text.replace('\r', '').split('\n');
     header = lines[0].split(',');
     rows = []
     for (var i = 1; i < lines.length; i++) {
-        if (lines[i].length > 0) {
-            row = CSVtoArray(lines[i]);
-            if (row == null) {
-                row = lines[i].replace('\r', '').split(',');
-            }
-            console.log(row);
-            rows.push(row);
+        line = lines[i];
+        if (line.length > 0) {
+            rows.push(line.split(regex));
         }
     }
-    console.log('done');
     callback(title, header, rows);
 }
-
 
 get = function(title, path) {
     getContentDiv().innerHTML = '<img class="loading" src="loading.gif" />';
