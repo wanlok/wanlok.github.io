495e9acc3e699e05001742831fc6d614729ecdce
Tung Lok Wan <wantunglok@gmail.com>
2023-12-05 03:00:45 +1100

    commit

--- research/github-security-policy/wanlok.js
+++ research/github-security-policy/wanlok.js
@@ -1,6 +1,6 @@
 leftWidth = 240;
 
-function prepareUI() {
+prepareUI = function() {
     div = document.getElementById(elementId);
     div.style.display = 'table';
     div.style.width = '100%';
@@ -20,7 +20,7 @@ function prepareUI() {
     rightDiv.children[0].style.overflowY = 'scroll';
 }
 
-function updateUI() {
+updateUI = function() {
     height = window.innerHeight;
     div = document.getElementById(elementId);
     div.style.height = height;
@@ -29,16 +29,16 @@ function updateUI() {
     div.children[1].children[0].style.height = height;
 }
 
-function prepareAndUpdateUI() {
+prepareAndUpdateUI = function() {
     prepareUI();
     updateUI();
 }
 
-function getContentDiv() {
+getContentDiv = function() {
     return document.getElementById(elementId).children[1].children[0];
 }
         
-function getCSV(csvText) {
+getCSV = function(csvText) {
     rows = [];
     lines = csvText.replace('\r', '').split('\n');
     columnNames = lines[0].split(',');
@@ -55,15 +55,66 @@ function getCSV(csvText) {
     return [columnNames, rows];
 }
 
-function get(title, path) {
+
+dummy2 = function(line) {
+    slices = line.split(',');
+    // if (columnNames.length == slices.length) {
+        // for (j = 0; j < slices.length; j++) {
+        //     row[columnNames[j]] = line[j];
+        // }
+        // rows.push(row);
+    // }
+
+}
+
+CSVtoArray = function(text) {
+    var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+    var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+    // Return NULL if input string is not well formed CSV string.
+    if (!re_valid.test(text)) return null;
+    var a = [];                     // Initialize array to receive values.
+    text.replace(re_value, // "Walk" the string using replace with callback.
+        function(m0, m1, m2, m3) {
+            // Remove backslash from \' in single quoted values.
+            if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+            // Remove backslash from \" in double quoted values.
+            else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+            else if (m3 !== undefined) a.push(m3);
+            return ''; // Return empty string.
+        });
+    // Handle special case of empty last value.
+    if (/,\s*$/.test(text)) a.push('');
+    return a;
+}
+
+write = function(title, text) {
+    lines = text.replace('\r', '').split('\n');
+    header = lines[0].split(',');
+    rows = []
+    for (var i = 1; i < lines.length; i++) {
+        if (lines[i].length > 0) {
+            row = CSVtoArray(lines[i]);
+            if (row == null) {
+                row = lines[i].replace('\r', '').split(',');
+            }
+            console.log(row);
+            rows.push(row);
+        }
+    }
+    console.log('done');
+    callback(title, header, rows);
+}
+
+
+get = function(title, path) {
+    getContentDiv().innerHTML = '<img class="loading" src="loading.gif" />';
     fetch(path).then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         return response.text();
-    }).then(responseText => {
-        tuple = getCSV(responseText);
-        callback(title, tuple[0], tuple[1]);
+    }).then(text => {
+        write(title, text);
     }).catch(error => {
         console.error('There was a problem fetching the CSV file:', error);
     });
